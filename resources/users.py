70b54253af01975ac42b0ddcef9442ab4e83b9f8
cnease10 <cierra.nease10@gmail.com>
2019-12-07 10:41:56 -0700

    registered 3 users & got the register route fixed to log the registering user in

--- resources/users.py
+++ resources/users.py
@@ -11,16 +11,19 @@ user = Blueprint('users', 'user')
 def register():
     payload = request.get_json()
     payload['username'].lower
+
+    if not payload['username'] or not payload['password']:
+        return jsonify(status=400)
     try:
         #query to check is username is already in use
-        models.User.get(models.User.username == payload['username'])
+        models.User.get(models.User.username ** payload['username'])
         return jsonify(data={}, status={"code": 401, "message": "Username taken"})
     except models.DoesNotExist:
         payload['password'] = generate_password_hash(payload['password'])
-        user = models.User.create(**payload)
+        new_user = models.User.create(**payload)
         #puts user in database
-        login_user(user) #starts session & logs them in
-        user_dict = model_to_dict(user)
+        login_user(new_user) #starts session & logs them in
+        user_dict = model_to_dict(new_user)
         del user_dict['password']  #delete password before returning request
         return jsonify(data=user_dict, status={"code": 200, "message": "User Created"})
 
