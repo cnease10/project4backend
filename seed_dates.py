e3c43e5fb231140cfa00f1fb8e51e6df34c84bf9
cnease10 <cierra.nease10@gmail.com>
2019-12-09 15:24:03 -0700

    re2: database

--- seed_dates.py
+++ seed_dates.py
@@ -5,13 +5,10 @@ with open('./date-ideas.csv', 'r') as f:
     for date_reader in reader:
         # print('Id:', date['date_id)
         date = {
-            'date_id': date_reader['date_id'].strip(),
+            # 'date_id': date_reader['date_id'].strip(),
             'name': date_reader['name'].strip(),
             'description': date_reader['description'].strip()
         }
         print(date)
-        try:
-            models.Date.create(**date)
-        except:
-            del date['date_id']
-            models.Date.create(**date)
+        # del date['date_id']
+        models.Date.create(**date)
